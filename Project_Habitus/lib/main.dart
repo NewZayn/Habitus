2fdb3ca05b03b0bd8930e8b436d484e4971405fa
aluno <aaaaaaa@gmail.com>
2024-05-14 20:13:08 -0300

    test

--- Project_Habitus/lib/main.dart
+++ Project_Habitus/lib/main.dart
@@ -1,27 +1,65 @@
+import 'dart:async';
+
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'login.dart';
-import 'home.dart';
+import 'firebase_options.dart';
+import 'pages/login_page.dart';
+import 'pages/main_page.dart';
+import 'pages/splash_page.dart';
+import 'pages/register_page.dart';
 
-void main() async {
+FirebaseAnalytics? analytics;
+
+FirebaseAnalyticsObserver? observer;
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // Parse Server initialization (if applicable)
-  // ...
-
+  FirebaseApp app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  if (kIsWeb) {
+    await FirebaseAuth.instanceFor(app: app).setPersistence(Persistence.LOCAL);
+  }
+  analytics = FirebaseAnalytics.instanceFor(app: app);
   runApp(MyApp());
 }
+
+const mainColor = Color(0xFF4672ff);
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Meu App',
+      title: 'Simple Login App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(mainColor),
+            minimumSize: MaterialStateProperty.all(
+              Size.fromHeight(60),
+            ),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+            ),
+          ),
+        ),
       ),
-      initialRoute: '/login',
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics!),
+      ],
+      initialRoute: '/',
       routes: {
-        '/home': (context) => const HomePage(),
-        '/login': (context) => const LoginPage(),
+        '/': (context) => SplashPage(),
+        '/login': (context) => LoginPage(),
+        '/main': (context) => MainPage(),
+        '/register': (context) => RegisterPage(),
       },
     );
   }
